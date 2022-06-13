fbfd61b551605a28046f565bf96efdc641721823
Federico Sacco <fedezac99@gmail.com>
2022-06-13 22:06:23 +0200

    todo show voxels on playground, else change second map

--- linorobot2/linorobot2_navigation/launch/navigation.launch.py
+++ linorobot2/linorobot2_navigation/launch/navigation.launch.py
@@ -22,7 +22,7 @@ from launch_ros.substitutions import FindPackageShare
 from launch_ros.actions import Node
 
 # Glboal variable to specify which map to be launched in rviz
-MAP_NAME='office_earthquake' 
+MAP_NAME='playground' 
 # Global variable to specify in a easier way which sensor to use for stvl layer
 SENSOR = 'rgbd' #3d or rgbd
 
